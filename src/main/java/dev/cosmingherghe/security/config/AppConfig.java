a73c61d09a05a0f3edd775661dd0f7dbdcd9ab9b
cosmingherghe <cosminlinux@gmail.com>
2023-01-05 23:26:51 -0500

    add AuthenticationProvider

--- src/main/java/dev/cosmingherghe/security/config/AppConfig.java
+++ src/main/java/dev/cosmingherghe/security/config/AppConfig.java
@@ -2,9 +2,12 @@ package dev.cosmingherghe.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import dev.cosmingherghe.security.dao.IUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,4 +25,18 @@ public class AppConfig {
         .orElseThrow(() -> new UsernameNotFoundException("UserName not found!"));
   }
 
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    
+    DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+    daoAuthProvider.setUserDetailsService(userDetailsService());
+    daoAuthProvider.setPasswordEncoder(passwordEncoder());
+
+    return daoAuthProvider;
+  }
+
+  @Bean
+  private PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
