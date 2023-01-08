71bfbc71aeb50a92e70f8370d65f18d1d8a66642
cosmingherghe <cosminlinux@gmail.com>
2023-01-07 22:03:31 -0500

    AuthenticationController class

---
+++ src/main/java/dev/cosmingherghe/security/auth/AuthenticationController.java
@@ -0,0 +1,29 @@
+package dev.cosmingherghe.security.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationService authService;
+  
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+
+    return ResponseEntity.ok(authService.register(request));
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequst request) {
+
+    return ResponseEntity.ok(authService.authenticate(request));   
+  }
+}
