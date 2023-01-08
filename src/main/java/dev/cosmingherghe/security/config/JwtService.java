9538b076f2f4cc98f96059aff3c9bac9505f87e4
cosmingherghe <cosminlinux@gmail.com>
2023-01-07 23:04:39 -0500

    update

--- src/main/java/dev/cosmingherghe/security/config/JwtService.java
+++ src/main/java/dev/cosmingherghe/security/config/JwtService.java
@@ -20,57 +20,57 @@ public class JwtService {
 
   private static final String SECRET_KEY = "576E5A7234753778214125442A472D4B6150645367556B58703273357638792F";
 
-  public String extractUserName(String jwToken) {
-    return extractClaim(jwToken, Claims::getSubject);
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
   }
 
-  private Claims extractAllClaims(String jwToken) {
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(
+      Map<String, Object> extraClaims,
+      UserDetails userDetails
+  ) {
     return Jwts
-      .parserBuilder()
-      .setSigningKey(getSignInKey())
-      .build()
-      .parseClaimsJws(jwToken)
-      .getBody();
+        .builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+        .parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
   }
 
   private Key getSignInKey() {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
-  }
-
-  //Extracting All Claims method
-  public <T> T extractClaim(String jwToken, Function<Claims, T> claimsResorver) {
-    final Claims claims = extractAllClaims(jwToken);
-    return claimsResorver.apply(claims);
-  }
-
-  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
-    return Jwts
-          .builder()
-          .setClaims(extraClaims)
-          .setSubject(userDetails.getUsername())
-          .setIssuedAt(new Date(System.currentTimeMillis()))
-          .setExpiration(new Date(System.currentTimeMillis() + 1000 + 60 * 24)) //24 hours 
-          .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-          .compact();
-  }
-
-  //Generating a Token without extra claims, only from the username
-  public String generateToken(UserDetails userDetails) {
-    return generateToken(new HashMap<>(), userDetails);
-  }
-
-  //Method that can validate a Token
-  public boolean isTokenValir(String jwToken, UserDetails userDetails) {
-    final String username = extractUserName(jwToken);
-    return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwToken));
-  }
-
-  private boolean isTokenExpired(String jwToken) {
-    return extractExpiration(jwToken).before(new Date());
-  }
-
-  private Date extractExpiration(String jwToken) {
-    return extractClaim(jwToken, Claims::getExpiration);
   }
 }
