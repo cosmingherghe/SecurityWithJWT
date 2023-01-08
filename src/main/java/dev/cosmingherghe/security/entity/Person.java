2c824746b09b0b02d08a6ce44651e32a16a75bf4
cosmingherghe <cosminlinux@gmail.com>
2023-01-07 21:21:13 -0500

    Person class

---
+++ src/main/java/dev/cosmingherghe/security/entity/Person.java
@@ -0,0 +1,32 @@
+package dev.cosmingherghe.security.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Table(name = "person")
+@Entity
+public class Person {
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String fullName;
+  private String email;
+  private String telephone;
+
+  // @OneToOne(cascade = CascadeType.ALL, 
+  //           mappedBy = "person", 
+  //           fetch = FetchType.LAZY)
+  // private User user;
+}
