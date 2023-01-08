4320629e88e54ccc3c99b716a74aeaf823fbf1a8
cosmingherghe <cosminlinux@gmail.com>
2023-01-07 22:03:09 -0500

    AuthenticationRequst class

---
+++ src/main/java/dev/cosmingherghe/security/auth/AuthenticationRequst.java
@@ -0,0 +1,16 @@
+package dev.cosmingherghe.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequst {
+
+  private String username;
+  private String password;
+}
