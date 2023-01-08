ddd2fb089aec3cc77f244915892c261559ffc07c
cosmingherghe <cosminlinux@gmail.com>
2023-01-07 21:31:33 -0500

    RegisterRequest class

---
+++ src/main/java/dev/cosmingherghe/security/auth/RegisterRequest.java
@@ -0,0 +1,19 @@
+package dev.cosmingherghe.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  private String username;
+  private String password;
+  private String fullName;
+  private String email;
+  private String telephone;
+}
